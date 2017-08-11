976f5205895a41635b880988b1c07424df2e2f2e
yamasy1549 <sanae@yamasy.info>
2017-08-12 00:12:01 +0900

    開講期間を追加

--- src/components/Curriculums.js
+++ src/components/Curriculums.js
@@ -1,45 +1,41 @@
 import React from 'react'
 import styles from './Curriculums.css'
 
-class Curriculum extends React.Component {
-  constructor(props, context) {
-    super(props, context)
-  }
+const Curriculum = ({ c, grade, toggleCredit }) => {
+  const { id, getCredit, credit, title } = c
+  const term = (c.term == 0) ? '通' : (c.term == 1) ? '前' : '後'
 
-  render() {
-    const { c, grade, toggleCredit } = this.props
-
-    return (
-      <li key={c.id}>
-        <label htmlFor={c.id}>
-          <input
-            id={c.id}
-            className={styles.curriculumCheck}
-            type="checkbox"
-            checked={c.getCredit}
-            onChange={(e) => {
-              toggleCredit(c.id, e.target.checked)
-            }}
-          />
-          <span className={styles.curriculum}>
-            <span className={styles.curriculumCredit}>{c.credit}</span>
-            <span className={styles.curriculumTitle}>{c.title}</span>
-            {(() => {
-              if(c.getCredit) {
-                return (
-                  <span className={styles.curriculumCheckmark}>
-                    <svg viewBox='0 0 36.9 28.3' enableBackground='new 0 0 36.9 28.3'>
-                      <path fill='#FFFFFF' d='M36.9,5.5c0,0.6-0.2,1.2-0.7,1.6L19,24.4l-3.2,3.2c-0.4,0.4-1,0.7-1.6,0.7c-0.6,0-1.2-0.2-1.6-0.7l-3.2-3.2l-8.6-8.6c-0.4-0.4-0.7-1-0.7-1.6c0-0.6,0.2-1.2,0.7-1.6l3.2-3.2c0.4-0.4,1-0.7,1.6-0.7s1.2,0.2,1.6,0.7l7,7L29.8,0.7c0.4-0.4,1-0.7,1.6-0.7C32,0,32.6,0.2,33,0.7l3.2,3.2C36.7,4.4,36.9,4.9,36.9,5.5z' />
-                    </svg>
-                  </span>
-                )
-              }
-            })()}
-          </span>
-        </label>
-      </li>
-    )
-  }
+  return (
+    <li key={id}>
+      <label htmlFor={id}>
+        <input
+          id={id}
+          className={styles.curriculumCheck}
+          type="checkbox"
+          checked={getCredit}
+          onChange={(e) => {
+            toggleCredit(id, e.target.checked)
+          }}
+        />
+        <span className={styles.curriculum}>
+          <span className={styles.curriculumCredit}>{credit}</span>
+          <span className={styles.curriculumTerm}>{term}</span>
+          <span className={styles.curriculumTitle}>{title}</span>
+          {(() => {
+            if(getCredit) {
+              return (
+                <span className={styles.curriculumCheckmark}>
+                  <svg viewBox='0 0 36.9 28.3' enableBackground='new 0 0 36.9 28.3'>
+                    <path fill='#FFFFFF' d='M36.9,5.5c0,0.6-0.2,1.2-0.7,1.6L19,24.4l-3.2,3.2c-0.4,0.4-1,0.7-1.6,0.7c-0.6,0-1.2-0.2-1.6-0.7l-3.2-3.2l-8.6-8.6c-0.4-0.4-0.7-1-0.7-1.6c0-0.6,0.2-1.2,0.7-1.6l3.2-3.2c0.4-0.4,1-0.7,1.6-0.7s1.2,0.2,1.6,0.7l7,7L29.8,0.7c0.4-0.4,1-0.7,1.6-0.7C32,0,32.6,0.2,33,0.7l3.2,3.2C36.7,4.4,36.9,4.9,36.9,5.5z' />
+                  </svg>
+                </span>
+              )
+            }
+          })()}
+        </span>
+      </label>
+    </li>
+  )
 }
 
 const Curriculums = ({ curriculums, toggleCredit }) => (
